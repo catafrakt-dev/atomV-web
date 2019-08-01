d8aa7304a762a95cf133654cdf9b7c73e39f36a9
catafrakt <catafrakt@gmail.com>
2019-08-01 17:50:03 +0300

    .

---
+++ src/auth/auth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './auth.scss';
+
+function Auth() {
+    return (
+        <div className="App">
+            <header className="App-header">
+               <p>Hello</p>
+            </header>
+        </div>
+    );
+}
+
+export default Auth;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import Auth from './auth/auth'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Route path="/" component={App} />
+            <Route path="/login" component={Auth} />
+            {/*<Route path="/registration" component={} />*/}
+            {/*<Route path="/test" component={} />*/}
+        </div>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
